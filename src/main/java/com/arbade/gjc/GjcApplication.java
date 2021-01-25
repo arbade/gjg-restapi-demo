75274586258e363bd977d0b81b90ec58ab51a392
arbade <ardabthn@gmail.com>
2021-01-25 20:44:31 +0300

    first commit

---
+++ src/main/java/com/arbade/gjc/GjcApplication.java
@@ -0,0 +1,13 @@
+package com.arbade.gjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjcApplication.class, args);
+	}
+
+}
